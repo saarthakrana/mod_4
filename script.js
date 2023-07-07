f9e44211156d4014f9869d83c8652131b36f8d8e
saarthakrana <sarthak.181202@gmail.com>
2023-07-07 11:03:36 +0530

    last

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-var names = new Array; 
-names[0] = "sarthak";
-names[1] = "John";
-names[2] = "Jason";
-names[3] = "sudeikis";
-names[4] = "paul";
-names[5] = "mollie";
-names[6] = "jacob";
-names[7] = "jenny";
-names[8] = "lol";
-names[9] = "end";
-console.log("abcde ");
-for (let i = 0; i < names.length; i++) {
-    if(names[i].charAt(0)=='J' || names[i].charAt(0)=='j'){
-        console.log("Goodbye " + names[i]);
-    }
-    else{
-        console.log("Hello " + names[i]);
-    }
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
 }
+
+})();
